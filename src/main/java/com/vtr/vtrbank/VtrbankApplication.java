3a382b12b434fa1bee1a24de8d44f68e3ffb9ae9
José Vitor <jose.vitorw018@gmail.com>
2026-07-21 23:27:17 -0300

    First commit

---
+++ src/main/java/com/vtr/vtrbank/VtrbankApplication.java
@@ -0,0 +1,13 @@
+package com.vtr.vtrbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtrbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VtrbankApplication.class, args);
+    }
+
+}

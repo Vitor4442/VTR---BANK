b3f49b4739bd1100575f91f7b94ec9419c56f400
José Vitor <jose.vitorw018@gmail.com>
2026-07-22 23:27:45 -0300

    User entity, DTO and Repo

---
+++ src/main/java/com/vtr/vtrbank/auth_users/repo/UserRepo.java
@@ -0,0 +1,10 @@
+package com.vtr.vtrbank.auth_users.repo;
+
+import com.vtr.vtrbank.auth_users.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}

697521a13d1fabcc8e610285deea50cafd39f2b7
José Vitor <jose.vitorw018@gmail.com>
2026-07-29 23:57:56 -0300

    Transaction DTO

---
+++ src/main/java/com/vtr/vtrbank/transaction/dtos/TransactionRequest.java
@@ -0,0 +1,20 @@
+package com.vtr.vtrbank.transaction.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.vtr.vtrbank.enums.TransactionType;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TransactionRequest {
+
+    private TransactionType transactionType;
+    private BigDecimal amount;
+    private String accountNumber;
+    private String description;
+
+    private String destinationAccountNumber; // The receiving account number if it's a transfer
+
+}
